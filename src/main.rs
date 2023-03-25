2d2e93096218ef1aa10e44b64ecb23f127223bde
ilir lika <49962713+1ilir0lika@users.noreply.github.com>
2023-03-25 17:30:12 +0100

    Add files via upload

---
+++ src/main.rs
@@ -0,0 +1,34 @@
+#[macro_use] extern crate rocket;
+
+use rocket::fs::FileServer;
+use rocket::form::Form;
+use rocket::fs::NamedFile;
+use teloxide::prelude::*;
+
+#[derive(FromForm)]
+struct Segnalazione {
+    segnalazioni: String,
+}
+
+
+#[post("/segnalazioni.html", data = "<form>")]
+async fn submit_segnalazione(form: Form<Segnalazione>) -> Option<NamedFile>{
+    let segnalazione = form.into_inner();
+    //mandare segnalazione a https://t.me/demorme_segnalazioni_bot
+    //pretty_env_logger::init();
+    let  chat_id:ChatId = ChatId(1755296389);
+    let bot = Bot::from_env();
+    match bot.send_message(chat_id, segnalazione.segnalazioni).await{
+        Err(_e)=>{NamedFile::open("fail.html").await.ok()},
+        _ => {NamedFile::open("success.html").await.ok()},
+    };
+    //log::info!("segnalazione inviata");
+    //aprire schermata bella di ringraziamento e mandare messaggio al bot telegram
+    NamedFile::open("success.html").await.ok()
+}
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+    .mount("/", routes![submit_segnalazione])
+    .mount("/",FileServer::from("/home/demor/progetti/site/demorme/"))
+}
